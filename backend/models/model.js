966938b133b1a84a4b29c0b3db52f061792ff28f
tzrph <TzRPheonix@gmail.com>
2023-03-29 14:10:10 +0200

    new backend

--- backend/models/model.js
+++ backend/models/model.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const dataSchema = new mongoose.Schema({
-    value: {
+    name: {
         required: true,
         type: String
     }
 })
 
-module.exports = mongoose.model('Data', dataSchema)
+module.exports = mongoose.model('userCollection', dataSchema)
--- backend/routes/route.js
+++ backend/routes/route.js
@@ -3,8 +3,18 @@ const router = express.Router();
 const Model = require('../models/model')
 
 //Post Method
-router.post('/post', (req, res) => {
-    res.send('Post API')
+router.post('/post', async (req, res) => {
+    console.log(req.body)
+    const data = new Model({
+        name: req.body.name,
+    })
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+    }
+    catch (error) {
+        res.status(400).json({message: error.message})
+    }
 })
 
 //Get all Method
